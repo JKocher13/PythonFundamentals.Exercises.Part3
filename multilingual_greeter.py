e0ab7ea654fa1870456d693334f4c9f93b32b301
JKocher13 <61708469+JKocher13@users.noreply.github.com>
2020-03-06 10:53:23 -0500

    Updated

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -1,13 +1,3 @@
-def greet (name,language):
-	"""
-	This function creates the greeting
-	""" 
-	if language == 1:
-		print ("Hello " + name)
-	elif language == 2:
-		print ("Hola " + name)
-	else:
-		print ("Bonjour " + name)
 
 def picklanguage():
 	language = int(input("What language do you speak? 1 for English, 2 for Spanish, 3 for French:  "))
@@ -19,14 +9,14 @@ def name_input(language):
 	This is a function that asks your name
 	"""
 	if language == 1:
-		name  = input("What is your name?")
-		print("Hello" + name) 	 
+		name  = input("What is your name?  ")
+		print("Hello " + name) 	 
 	elif language == 2:
-		name = input("Como se llama?")
-		print("Hola" + name)
+		name = input("Como se llama?  ")
+		print("Hola " + name)
 	elif language == 3:
-		name = input("Comment vous appelez-vous?")
-		print ("Bonjour" + name)
+		name = input("Comment vous appelez-vous?  ")
+		print ("Bonjour " + name)
 	else:
 		print("Didn't select an option!")
 		
